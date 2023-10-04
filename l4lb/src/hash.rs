ecde31e2957cea0942aadf27a041ae642dd630ac
Isaac Diamond <isaacd9@gmail.com>
2023-10-04 09:15:10 -0700

    .

--- l4lb/src/hash.rs
+++ l4lb/src/hash.rs
@@ -16,11 +16,16 @@ impl SimpleConsistentHasher {
 
 impl ConsistentHasher for SimpleConsistentHasher {
     fn generate_hash_ring(&self, reals: &[RealServer]) -> Vec<RealServer> {
+        let chunk_size = self.ring_size / reals.len() as u32;
         let mut ring = Vec::with_capacity(self.ring_size as usize);
 
-        for i in 0..self.ring_size {
-            let j = i % reals.len() as u32;
-            ring.push(reals[j as usize]);
+        for i in 1..self.ring_size + 1 {
+            let mut chunk = i / chunk_size;
+            // If we're at the end of the ring, we need to wrap around
+            if chunk >= reals.len() as u32 {
+                chunk = reals.len() as u32 - 1;
+            }
+            ring.push(reals[chunk as usize]);
         }
 
         ring
